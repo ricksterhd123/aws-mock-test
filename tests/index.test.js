c613ce6d2cd3a752b42b626db1678ee57a5d80b8
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-07 23:37:44 +0000

    Set SDK outside test cases

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 const AWSMock = require('aws-sdk-mock');
+AWSMock.setSDK('../aws-sdk');
 
 test('Can read file from S3', async () => {
-    AWSMock.setSDK('../aws-sdk');
     AWSMock.mock('S3', 'getObject', (params, callback) => {
         callback(null, {
             Body: 'hello world',
@@ -13,5 +13,17 @@ test('Can read file from S3', async () => {
     const result = await readFileFromS3('test', 'test.json');
 
     expect(result).toBe('hello world');
+    AWSMock.restore('S3');
+});
+
+test('Fails gracefully if failed to read file from S3', async () => {
+    AWSMock.mock('S3', 'getObject', (params, callback) => {
+        callback(new Error('Failed to read file'), null);
+    });
+
+    const { readFileFromS3 } = require('../src');
+    const result = await readFileFromS3('test', 'test.json');
+
+    expect(result).toBe(false);
     AWSMock.restore('S3');
 });
