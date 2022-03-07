94a226b027c5cc11522f51c2eaf43b3ed0245c61
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-07 23:36:58 +0000

    Wrap s3 getObject in try/catch blocks to fail gracefully

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const { S3 } = require('aws-sdk');
 
 async function readFileFromS3(bucket, key) {
     const s3Client = new S3();
-    const result = await s3Client.getObject({
-        Bucket: bucket,
-        Key: key
-    }).promise();
-
-    return result.Body;
+    try {
+        const result = await s3Client.getObject({
+            Bucket: bucket,
+            Key: key
+        }).promise();
+        return result.Body;
+    } catch (error) {
+        return false;
+    }
 }
 
 module.exports = {
